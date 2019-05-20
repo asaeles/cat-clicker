0f0f7574adfff232df2ae9ff066bea49d07855d2
Ahmad Sulaeman <asaeles@gmail.com>
2019-05-20 04:29:10 +0200

    Add list of cats
    Now the app displays a list of cats instead of
    one cat.
    
    Although cats.json has been added, it is not used
    becuase of cross site restrictions

--- js/app.js
+++ js/app.js
@@ -1,13 +1,45 @@
-function ViewModel() {
-  this.name = ko.observable("Lilly");
-  this.url = ko.observable(
-    "https://hairstylecamp.com/wp-content/uploads/cat-1.jpg"
-  );
-  this.clicks = ko.observable(0);
+var cats = [
+  {
+    name: "Luna",
+    clicks: 0,
+    nicknames: ["Moon", "Puusy"],
+    url:
+      "http://www.pethealthnetwork.com/sites/default/files/how-do-i-trim-my-kittens-nails524581937.jpg"
+  },
+  {
+    name: "Chloe",
+    clicks: 0,
+    nicknames: ["Cool", "Coco"],
+    url:
+      "http://sites.psu.edu/siowfa15/wp-content/uploads/sites/29639/2015/10/cat.jpg"
+  },
+  {
+    name: "Bella",
+    clicks: 0,
+    nicknames: ["Beauty", "B"],
+    url: "https://hairstylecamp.com/wp-content/uploads/cat-1.jpg"
+  },
+  {
+    name: "Lucy",
+    clicks: 0,
+    nicknames: ["Lucky", "Lulu"],
+    url: "http://funlava.com/wp-content/uploads/2013/03/cute-cats-wallpaper.jpg"
+  },
+  {
+    name: "Lily",
+    clicks: 0,
+    nicknames: ["Rose", "Love"],
+    url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQX7sbQ24kc_kL53W7xQNvio_991tFpoPp-mMZ5Dn9LMo_4h6Sm"
+  }
+];
 
-  this.incrClicks = function() {
-    this.clicks(this.clicks() + 1);
-  };
+var Cat = function(data) {
+  var self = this;
+  this.name = ko.observable(data.name);
+  this.url = ko.observable(data.url);
+  this.clicks = ko.observable(data.clicks);
+  this.nicknames = ko.observableArray(data.nicknames);
 
   this.title = ko.computed(function() {
     if (this.clicks() < 10) {
@@ -26,8 +58,28 @@ function ViewModel() {
       return "Elderly";
     }
   }, this);
-}
+};
+
+var ViewModel = function() {
+  var self = this;
+
+  self.catsList = ko.observableArray([]);
+  cats.forEach(cat => {
+    self.catsList().push(new Cat(cat));
+  });
+
+  self.setCurrCat = function(cat) {
+    self.currCat(cat);
+  };
+
+  self.currCat = ko.observable(self.catsList()[0]);
+
+  self.incrClicks = function() {
+    self.currCat().clicks(self.currCat().clicks() + 1);
+  };
+};
 
 window.addEventListener("load", function() {
-  ko.applyBindings(new ViewModel());
+  viewModel = new ViewModel();
+  ko.applyBindings(viewModel);
 });
